894e87463e554c076ab0965006f7e02463011ccb
Sharbel Chris <sharbel.cosa@gmail.com>
2019-12-19 11:03:40 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## React Codelab  [![time tracker](https://wakatime.com/badge/github/sharbel93/react-codelabs.svg)](https://wakatime.com/badge/github/sharbel93/react-codelabs)
+
+This is a repository that has projects built by react and other technologies just for coding practices 
+
+
---
+++ monsters-showcase/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { CardList } from "./components/card-list/card-list.component";
+import './App.css';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+       monsters: [],
+       searchField: ''
+    };
+
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({ monsters: users }));
+  }
+
+  render() {
+    return (
+        <div className="App">
+            <input type='search' placeholder='search monsters'
+                   onChange={e => this.setState({ searchField: e.target.value })}/>
+          {/* props === monsters */}
+          <CardList monsters={this.state.monsters} />
+        </div>
+    )
+  }
+}
+
+export default App;
